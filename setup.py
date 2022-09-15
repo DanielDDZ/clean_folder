6750919f14c406d0fddd75734dcc2f749ff22703
Daniel Zubov <danielzubov12@gmail.com>
2022-09-15 13:18:49 +0300

    add files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='clean',
+      version='1.0',
+      description='Sorts files in a folder',
+      url='https://github.com/DanielDDZ/clean_folder',
+      author='Daniel Zubov',
+      author_email='danielzubov12@gmail.com',
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': [
+          'clean_folder = clean_folder.clean:main']}
+      )
